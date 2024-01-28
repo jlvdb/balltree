7d0f5a6d102ae3daafd3c68f3ae767189cbf50d6
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2024-01-28 15:18:41 +0100

    added codestyle through isort, black and flake8

--- balltree/__init__.py
+++ balltree/__init__.py
@@ -1,3 +1,10 @@
 __version__ = "0.1"
+
 from ._balltree import BallTree, default_leafsize
 from .angulartree import AngularTree
+
+__all__ = [
+    "AngularTree",
+    "BallTree",
+    "default_leafsize",
+]
