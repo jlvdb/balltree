c97b513fcb0a3f678b7f2bc4438f99089bc2862f
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2024-01-29 20:19:40 +0100

    fixed bugs in AngularTree and added missing tests

--- balltree/angulartree.py
+++ balltree/angulartree.py
@@ -20,13 +20,15 @@ class AngularTree:
         weight: NDArray | None = None,
         leafsize: int = default_leafsize,
     ) -> None:
-        xyz = coord.radec_to_xyz(radec)
+        xyz = coord.angular_to_euclidean(radec)
         self._tree = BallTree(xyz, weight, leafsize=leafsize)
 
     @property
     def data(self) -> NDArray[np.float64]:
         data = self._tree.data
-        radec = coord.xy_to_radec(np.transpose([data["x"], data["y"], data["z"]]))
+        radec = coord.euclidean_to_angular(
+            np.transpose([data["x"], data["y"], data["z"]])
+        )
 
         dtype = [("ra", "f8"), ("dec", "f8"), ("weight", "f8")]
         array = np.empty(len(data), dtype=dtype)
@@ -49,11 +51,18 @@ class AngularTree:
 
     @property
     def center(self) -> tuple(float, float, float):
-        return tuple(coord.xy_to_radec(self._tree.center)[0])
+        return tuple(coord.euclidean_to_angular(self._tree.center)[0])
 
     @property
     def radius(self) -> float:
-        return coord.radius_to_angle(self._tree.radius)
+        center_xyz = coord.angular_to_euclidean(self.center)[0]
+        data_xyz = self._tree.data
+        # compute the maximum distance from the center project one the sphere
+        dx = data_xyz["x"] - center_xyz[0]
+        dy = data_xyz["y"] - center_xyz[1]
+        dz = data_xyz["z"] - center_xyz[2]
+        dist = np.sqrt(dx * dx + dy * dy + dz * dz)
+        return coord.chorddist_to_angle(dist.max())
 
     @classmethod
     def from_random(
@@ -64,11 +73,11 @@ class AngularTree:
         dec_max: float,
         size: int,
     ) -> AngularTree:
-        x_min, y_min = coord.radec_to_xy(ra_min, dec_min)
-        x_max, y_max = coord.radec_to_xy(ra_max, dec_max)
+        ((x_min, y_min),) = coord.angular_to_cylinder([ra_min, dec_min])
+        ((x_max, y_max),) = coord.angular_to_cylinder([ra_max, dec_max])
         x = np.random.uniform(x_min, x_max, size)
         y = np.random.uniform(y_min, y_max, size)
-        radec = coord.xy_to_radec(np.transpose([x, y]))
+        radec = coord.cylinder_to_angular(np.transpose([x, y]))
         return cls(radec)
 
     @classmethod
@@ -92,8 +101,8 @@ class AngularTree:
         angle: float,
         weight: NDArray | None = None,
     ) -> float:
-        xyz = coord.radec_to_xyz(radec)
-        radius = coord.angle_to_radius(angle)
+        xyz = coord.angular_to_euclidean(radec)
+        radius = coord.angle_to_chorddist(angle)
         return self._tree.count_radius(xyz, radius, weight)
 
     def count_range(
@@ -103,9 +112,9 @@ class AngularTree:
         ang_max: float,
         weight: NDArray | None = None,
     ) -> float:
-        xyz = coord.radec_to_xyz(radec)
-        rmin = coord.angle_to_radius(ang_min)
-        rmax = coord.angle_to_radius(ang_max)
+        xyz = coord.angular_to_euclidean(radec)
+        rmin = coord.angle_to_chorddist(ang_min)
+        rmax = coord.angle_to_chorddist(ang_max)
         return self._tree.count_range(xyz, rmin, rmax, weight)
 
     def dualcount_radius(
@@ -113,8 +122,10 @@ class AngularTree:
         other: AngularTree,
         angle: float,
     ) -> float:
-        radius = coord.angle_to_radius(angle)
-        return self._tree.dualcount_radius(other, radius)
+        if not isinstance(other, self.__class__):
+            raise TypeError("'other' must be of type 'AngularTree'")
+        radius = coord.angle_to_chorddist(angle)
+        return self._tree.dualcount_radius(other._tree, radius)
 
     def dualcount_range(
         self,
@@ -122,6 +133,8 @@ class AngularTree:
         ang_min: float,
         ang_max: float,
     ) -> float:
-        rmin = coord.angle_to_radius(ang_min)
-        rmax = coord.angle_to_radius(ang_max)
-        return self._tree.dualcount_range(other, rmin, rmax)
+        if not isinstance(other, self.__class__):
+            raise TypeError("'other' must be of type 'AngularTree'")
+        rmin = coord.angle_to_chorddist(ang_min)
+        rmax = coord.angle_to_chorddist(ang_max)
+        return self._tree.dualcount_range(other._tree, rmin, rmax)
--- tests/test_angulartree.py
+++ tests/test_angulartree.py
@@ -1,67 +1,124 @@
+import numpy as np
+import numpy.testing as npt
 import pytest
 
-# from balltree.angulartree import AngularTree
+from balltree import coordinates, default_leafsize
+from balltree.angulartree import AngularTree
+
+
+@pytest.fixture
+def mock_data():
+    return np.deg2rad(
+        [
+            [0.0, 0.0],
+            [90.0, 0.0],
+            [180.0, 0.0],
+            [270.0, 0.0],
+            [0.0, 90.0],
+            [0.0, -90.0],
+        ]
+    )
+
+
+@pytest.fixture
+def mock_data_small():
+    return np.deg2rad(
+        [
+            [0.0, -45.0],
+            [0.0, 0.0],
+            [0.0, 45.0],
+        ]
+    )
+
+
+@pytest.fixture
+def mock_tree(mock_data):
+    return AngularTree(mock_data)
+
+
+def data_to_view(data, weight=True):
+    dtype = [("ra", "f8"), ("dec", "f8"), ("weight", "f8")]
+    data = np.atleast_2d(data)
+    array = np.empty(len(data), dtype=dtype)
+    array["ra"] = data[:, 0]
+    array["dec"] = data[:, 1]
+    array["weight"] = weight if weight is not None else 1.0
+    return array
 
 
 class TestAngularTree:
-    def test_init(self):
-        pass
+    def test_init(self, mock_data):
+        tree = AngularTree(mock_data)
+        xyz_tree = tree._tree.data
+        xyz_data = coordinates.angular_to_euclidean(mock_data)
+        npt.assert_almost_equal(xyz_data[:, 0], xyz_tree["x"])
+        npt.assert_almost_equal(xyz_data[:, 1], xyz_tree["y"])
+        npt.assert_almost_equal(xyz_data[:, 2], xyz_tree["z"])
 
-    @pytest.mark.skip
-    def test_data(self):
-        pass
+    def test_data(self, mock_tree, mock_data):
+        npt.assert_array_equal(mock_tree.data, data_to_view(mock_data))
 
-    @pytest.mark.skip
-    def test_num_data(self):
-        pass
+    def test_num_data(self, mock_tree, mock_data):
+        assert mock_tree.num_data == len(mock_data)
 
-    @pytest.mark.skip
-    def test_leafsize(self):
-        pass
+    def test_leafsize(self, mock_tree):
+        assert mock_tree.leafsize == default_leafsize
 
-    @pytest.mark.skip
-    def test_sum_weight(self):
-        pass
+    def test_sum_weight(self, mock_tree):
+        assert mock_tree.sum_weight == np.ones(mock_tree.num_data).sum()
 
-    @pytest.mark.skip
-    def test_center(self):
-        pass
+    def test_center(self, mock_data_small):
+        tree = AngularTree(mock_data_small)
+        npt.assert_array_almost_equal(tree.center, np.median(mock_data_small, axis=0))
 
-    @pytest.mark.skip
-    def test_radius(self):
-        pass
+    def test_radius(self, mock_data_small):
+        tree = AngularTree(mock_data_small)
+        npt.assert_almost_equal(tree.radius, np.deg2rad(45))
 
-    @pytest.mark.skip
     def test_from_random(self):
-        pass
+        limit = 1.0
+        size = 10000
+        tree = AngularTree.from_random(0, limit, -limit, limit, size)
+        data = tree.data
+        assert tree.num_data == size
+        assert data["ra"].min() >= 0.0
+        assert data["ra"].max() <= limit
+        assert data["dec"].min() >= -limit
+        assert data["dec"].max() <= limit
 
-    @pytest.mark.skip
-    def test_from_file(self):
-        pass
+    def test_to_from_file(self, mock_data, tmp_path):
+        fpath = str(tmp_path / "tree.dump")
+        orig = AngularTree(mock_data, leafsize=4)
+        orig.to_file(fpath)
 
-    @pytest.mark.skip
-    def test_to_file(self):
-        pass
+        restored = AngularTree.from_file(fpath)
+        assert orig.leafsize == restored.leafsize
+        assert orig.num_data == restored.num_data
+        assert orig.count_nodes() == restored.count_nodes()
+        npt.assert_array_equal(orig.data, restored.data)
 
-    @pytest.mark.skip
-    def test_count_nodes(self):
-        pass
+    def test_count_nodes(self, mock_data):
+        assert AngularTree(mock_data, leafsize=4).count_nodes() == 3
 
-    @pytest.mark.skip
-    def test_get_node_data(self):
-        pass
-
-    @pytest.mark.skip
-    def test_count_radius(self):
-        pass
+    def test_count_radius(self, mock_tree):
+        point = (0.0, 0.0, 0.0)
+        eps = 1e-9
+        radius = np.pi / 2.0
+        assert mock_tree.count_radius(point, radius - eps) == 1
+        assert mock_tree.count_radius(point, radius + eps) == 5
+        assert mock_tree.count_radius(point, 2.0 * radius + eps) == 6
 
     @pytest.mark.skip
     def test_count_range(self):
         pass
 
-    @pytest.mark.skip
-    def test_dualcount_radius(self):
-        pass
+    def test_dualcount_radius(self, mock_tree):
+        eps = 1e-9
+        radius = np.pi / 2.0
+        N = mock_tree.num_data
+        assert mock_tree.dualcount_radius(mock_tree, radius - eps) == 1 * N
+        assert mock_tree.dualcount_radius(mock_tree, radius + eps) == 5 * N
+        assert mock_tree.dualcount_radius(mock_tree, 2.0 * radius + eps) == 6 * N
 
     @pytest.mark.skip
     def test_dualcount_range(self):
